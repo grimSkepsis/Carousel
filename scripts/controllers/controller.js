2cc98d7f0cd10cb63b2b27f70211f8680de1b616
Michael Hofer <mhofer180993@gmail.com>
2015-02-08 13:04:03 -0800

    Added backward and forward cycling

--- scripts/controllers/controller.js
+++ scripts/controllers/controller.js
@@ -3,8 +3,8 @@ app.controller('CarouselController',function ($scope){
   $scope.imgUrlArray = ["http://blog.flametreepublishing.com/Portals/293166/images/cthulhu-414web.jpg",
                         "http://fc00.deviantart.net/fs70/i/2011/142/8/f/dagon_by_velvetcat-d3gxr0w.jpg",
                         "http://fc04.deviantart.net/fs70/i/2013/319/b/2/yog_sothoth_rising_by_butttornado-d6ubvy6.jpg"];
-  $scope.initCarousel = function() {
-
-  };
+  $scope.direction = "backward";
+  $scope.cycle = true;
+  $scope.transitionTime = 2000;
 
 });
--- scripts/directives/carousel.js
+++ scripts/directives/carousel.js
@@ -4,14 +4,67 @@ angular.module('app.directives.carousel', [])
       restrict: 'E',//restricts directive to Element A = Attr  C = class
       scope: {
         data: '=',
-
+        dir: '=',
+        cycle:'=',
+        time: '='
       },
-      controller: function($scope){
+      controller: function($scope, $interval){
+        $scope.carouselArray = new Array();
+        var cycleDir = $scope.dir;
+        var cycle = $scope.cycle;
+        var cArray = $scope.carouselArray;
+        var transTime = $scope.time;
+        var positionTracker = {cur: 0, next: 1, prev: $scope.data.length -1};
 
+        for(var i = 0; i < $scope.data.length; i++){
+          if(i == 0){
+            cArray.push({url: $scope.data[i], displayed: true});
+          }
+          else{
+            cArray.push({url: $scope.data[i], displayed: false});
+          }
+        }
+
+        $scope.advanceForward = function(){
+          cArray[positionTracker.next].displayed = true;
+          cArray[positionTracker.cur].displayed = false;
+
+          positionTracker.prev = positionTracker.cur;
+          positionTracker.cur = positionTracker.next;
+          positionTracker.next++;
+
+          if(positionTracker.next > cArray.length-1){
+            positionTracker.next = 0;
+          }
+        };
+
+        $scope.advanceBackward = function(){
+          cArray[positionTracker.prev].displayed = true;
+          cArray[positionTracker.cur].displayed = false;
+
+          positionTracker.next = positionTracker.cur;
+          positionTracker.cur = positionTracker.prev;
+          positionTracker.prev--;
+
+          if(positionTracker.prev < 0){
+            positionTracker.prev = cArray.length-1;
+          }
+        };
+
+
+        $interval(function(){
+          if(cycle){
+            if(cycleDir == 'forward'){
+              $scope.advanceForward();
+            }else if(cycleDir =='backward'){
+              $scope.advanceBackward();
+            }
+         }
+       },transTime);
       },
       link: function(scope, element, attribute) {
         var imgArray = element.children;
-        console.log(element.context.children);
+
       },
       templateUrl:"templates/carousel.html"
     };
